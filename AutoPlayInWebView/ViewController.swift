ac26abd2b0df6058d261e3f5b6de680298b87832
tshiraishi <tshiraishi@mushikago.com>
2015-06-19 10:03:35 +0900

    Step1End

--- AutoPlayInWebView/ViewController.swift
+++ AutoPlayInWebView/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    var _prefix:String = "autoplaytest://"
+    
+    @IBOutlet weak var wv: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let _path:String = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "sound")!
+        wv.loadRequest(NSURLRequest(URL: NSURL(string: _path)!))
+        
+        wv.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if let _urlstr:String = request.URL?.absoluteString{
+            if(_urlstr.hasPrefix(_prefix)){
+                let _param = _urlstr.stringByReplacingOccurrencesOfString(_prefix, withString: "")
+                wv.stringByEvaluatingJavaScriptFromString("playAudioFn('" + _param + "')")
+                
+                return Bool(false)
+            }
+        }
+        return Bool(true)
+    }
 
 }
 
