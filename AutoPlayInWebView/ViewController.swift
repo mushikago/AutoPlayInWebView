236341a2ab61ff083656d90b8ed8871807f8f130
tshiraishi <tshiraishi@mushikago.com>
2015-08-10 17:58:26 +0900

    wv.mediaPlaybackRequiresUserAction = false
    この一行で自動再生可能。

--- AutoPlayInWebView/ViewController.swift
+++ AutoPlayInWebView/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         let _path:String = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "sound")!
         wv.loadRequest(NSURLRequest(URL: NSURL(string: _path)!))
-        
+//        wv.allowsInlineMediaPlayback = true;
+        wv.mediaPlaybackRequiresUserAction = false;
         wv.delegate = self
     }
 
@@ -28,17 +29,17 @@ class ViewController: UIViewController, UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        if let _urlstr:String = request.URL?.absoluteString{
-            if(_urlstr.hasPrefix(_prefix)){
-                let _param = _urlstr.stringByReplacingOccurrencesOfString(_prefix, withString: "")
-                wv.stringByEvaluatingJavaScriptFromString("playAudioFn('" + _param + "')")
-                
-                return Bool(false)
-            }
-        }
-        return Bool(true)
-    }
+//    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+//        if let _urlstr:String = request.URL?.absoluteString{
+//            if(_urlstr.hasPrefix(_prefix)){
+//                let _param = _urlstr.stringByReplacingOccurrencesOfString(_prefix, withString: "")
+//                wv.stringByEvaluatingJavaScriptFromString("playAudioFn('" + _param + "')")
+//                
+//                return Bool(false)
+//            }
+//        }
+//        return Bool(true)
+//    }
 
 }
 
